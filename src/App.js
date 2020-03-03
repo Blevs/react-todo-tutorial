ccb8a26faebe942965ce7accea214581915b296b
Henry Blevins <heblevi@gmail.com>
2020-03-02 23:30:29 -0500

    Finished recitation

--- src/App.js
+++ src/App.js
@@ -27,15 +27,85 @@ const initialTodos = [
 
 const App = () => {
   const [todosArray, setTodosArray] = useState(initialTodos);
+
+  const toggleTodo = (todo) => {
+    setTodosArray(todosArray.map(t => {
+      return t.id === todo.id ? { ...t, done: !t.done } : t;
+    }));
+  };
+
+  const addTodo = (text) => {
+    if (text !== '') {
+      setTodosArray([{
+        id: Date.now(),
+        title: text,
+        done: false,
+      }, ...todosArray]); 
+    }
+  };
+
   return (
     <div>
       <header className={styles.header}>
         <h1>TODO Tracker</h1>
       </header>
       <main className={styles.main}>
-        TODO: TODO's
+        <TodoForm addTodo={addTodo} />
+        <TodoList>
+          {todosArray.map(todo => (
+            <Todo key={todo.id} todo={todo} toggleTodo={toggleTodo} />
+          ))}
+        </TodoList>
       </main>
     </div>
+  );
+};
+
+const TodoList = ({ children }) => {
+  return (
+    <div className="todoList">
+      {children}
+    </div>
+  );
+};
+
+const Todo = ({ todo, toggleTodo }) => {
+  return (
+    <div
+      key={todo.id}
+      className={cn(styles.todo, todo.done && styles.done)}
+      onClick={() => toggleTodo(todo)}
+    >
+      {todo.title}
+    </div>
+  );
+};
+
+const TodoForm = ({ addTodo }) => {
+  const [todoText, setTodoText] = useState('');
+
+  return (
+    <form
+      className={styles.todoForm}
+      onSubmit={event => {
+        event.preventDefault();
+        addTodo(todoText);
+        setTodoText('');
+      }}>
+      <input
+        className={styles.todoInput}
+        name="todo"
+        value={todoText}
+        onChange={event => {
+          setTodoText(event.target.value);
+        }}
+      />
+      <button type="submit"
+              className={styles.add}
+      >
+        Add
+      </button>
+    </form>
   );
 };
 
